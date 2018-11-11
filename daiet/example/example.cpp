dbc0eab9b5d45ad5f5b2c98231e3ff73b28f8729
AmedeoSapio <amedeo.sapio@gmail.com>
2018-11-11 16:44:01 +0000

    Improved example

--- daiet/example/example.cpp
+++ daiet/example/example.cpp
@@ -2,22 +2,39 @@
 #include <fstream>
 #include <signal.h>
 #include <chrono>
+#include <thread> 
 #include <daiet/DaietContext.hpp>
 
 using namespace daiet;
 using namespace std;
 
+    DaietContext* ctx;
+
+void signal_handler(int signum) {
+    if (signum == SIGINT || signum == SIGTERM) {
+        cout << " Signal " << signum << " received, preparing to exit...";
+        delete ctx;
+        exit(EXIT_SUCCESS);
+    }
+}
+
 int main() {
 
-    DaietContext ctx;
+    ctx = new DaietContext();
+    sleep(2);
 
     int count = 10485760 * 32;
+    int num_rounds = 5;
     int num_workers = 2;
     int faulty = 0;
 
     int32_t* p = new int32_t[count];
 
-    for (int jj = 1; jj <= 5; jj++) {
+    /* Set signal handler */
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
+
+    for (int jj = 1; jj <= num_rounds; jj++) {
 
         std::cout << "INT round " << jj << std::endl;
 
@@ -27,7 +44,10 @@ int main() {
             p[i] = -(jj * i);
 
         auto begin = std::chrono::high_resolution_clock::now();
-        ctx.AllReduceInt32(p, count);
+        if (!ctx->try_daiet(p, count,1)){
+            cout << "Daiet failed";
+            exit(EXIT_FAILURE);
+        }
         auto end = std::chrono::high_resolution_clock::now();
 
         for (int i = 0; i < count; i++) {
@@ -35,13 +55,13 @@ int main() {
                 faulty++;
         }
 
-        std::cout << "Done INT round " << jj << ": Faulty: " << faulty << " Time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count()
-                << " ns" << std::endl;
+        std::cout << "Done INT round " << jj << ": Faulty: " << faulty << " Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()
+                << " ms" << std::endl;
     }
 
     float* fp = new float[count];
 
-    for (int jj = 1; jj <= 4; jj++) {
+    for (int jj = 1; jj <= num_rounds; jj++) {
 
         std::cout << "FLOAT round " << jj << std::endl;
 
@@ -51,7 +71,11 @@ int main() {
             fp[i] = -0.1 * (jj * i);
 
         auto begin = std::chrono::high_resolution_clock::now();
-        ctx.AllReduceFloat(fp, count);
+        if (!ctx->try_daiet(fp, count,1)){
+            cout << "Daiet failed";
+            exit(EXIT_FAILURE);
+        }
+
         auto end = std::chrono::high_resolution_clock::now();
 
         for (int i = 0; i < count; i++) {
@@ -60,7 +84,7 @@ int main() {
         }
 
         std::cout << "Done FLOAT round " << jj << ": Faulty: " << faulty << " Time: "
-                << std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() << " ns" << std::endl;
+                << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << " ms" << std::endl;
     }
 
     /*
@@ -71,5 +95,7 @@ int main() {
      }
      myfile.close();
      */
+
+    delete ctx;
     exit(EXIT_SUCCESS);
 }
